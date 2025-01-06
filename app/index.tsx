97e9c7336a4fe0445587d3f15d7829dbdfd5b770
SethyRung <rungsethyhk@gmail.com>
2025-01-06 20:05:33 +0700

    feat: initialize project

---
+++ app/index.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text className="text-primary-300 font-rubik text-xl">Home screen.</Text>
+    </View>
+  );
+}
